23d57cc19860080935743e971c7639eb67285bf0
FilipBerka04 <filipberka4@gmail.com>
2026-05-15 15:21:02 +0200

    Added ^C (SIGINT) handling for the runner shell

--- hello/main.c
+++ hello/main.c
@@ -4,5 +4,9 @@
 int main(int argc, char* argv[])
 {
     printf("Hello, I'm just here for testing\n");
+    while (1) {
+	printf("OwO\n");
+	sleep(1);
+    }
     return 0;
 }
--- runner/main.c
+++ runner/main.c
@@ -1,28 +1,44 @@
 // "shell" for running c programs from this directory
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
+int child_pid = 0;
 
 void run(const char* path){
-    int rc = fork();
-    if(rc < 0){
+    child_pid = fork();
+    if(child_pid < 0){
 	printf("Error! Coulnd't create a process!\n");
 	exit(1);
-    }else if(rc == 0){
+    }else if(child_pid == 0){
 	char* args[] = {NULL};
 	execve(path, args, NULL);
     }else{
 	int status;
-	wait(&status);
+	waitpid(child_pid, &status, WUNTRACED);
 	printf("=======================\n");
 	printf("The program finished running with return code: %d\n", WEXITSTATUS(status));
+	child_pid = 0;
     }
+}
+
+void kill_child(){
+    if(child_pid == 0){
+	printf("\nClosing runnner shell...\n");
+	exit(0);
+    }
+    child_pid = 0;
 }
 
 int main(int argc, char *argv[])
 {
+    struct sigaction action;
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = kill_child;
+    sigaction(SIGINT, &action, NULL);
     printf("Welcome to the runner shell!\n");
     // main loop
     while(1){
