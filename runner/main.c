64a0a3a52569f1cec68daeaf989aa40efe0e61b0
FilipBerka04 <filipberka4@gmail.com>
2026-05-17 14:00:20 +0200

    Added a prompt allowing for running any program in apps/ directory

--- runner/main.c
+++ runner/main.c
@@ -6,16 +6,24 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+#define PATH "./apps/"
+
+
 int child_pid = 0;
 
-void run(const char* path){
+void run(const char* app){
+    char path[64] = { PATH };
+    strcat(path, app);
     child_pid = fork();
     if(child_pid < 0){
 	printf("Error! Coulnd't create a process!\n");
 	exit(1);
     }else if(child_pid == 0){
 	char* args[] = {NULL};
+	printf("=======================\n");
 	execve(path, args, NULL);
+	printf("ERRORORRORR!!!!!!\n");
+	exit(-1);
     }else{
 	int status;
 	waitpid(child_pid, &status, WUNTRACED);
@@ -35,17 +43,19 @@ void kill_child(){
 
 int main(int argc, char *argv[])
 {
+    // handle ctrl-c
     struct sigaction action;
     memset(&action, 0, sizeof(action));
     action.sa_handler = kill_child;
     sigaction(SIGINT, &action, NULL);
+
+    char buff[64];
     printf("Welcome to the runner shell!\n");
     // main loop
     while(1){
-	printf("Press Enter to run the test script");
-	getchar();
-	printf("=======================\n");
-	run("./hello/builds/hello");
+	printf("&");
+	scanf("%s", buff);
+	run(buff);
     } 
     return 0;
 }
